4593611cbc6686c84bbd47637f993f9fe23cc086
alexfyh <alexander.fyh@gmail.com>
2019-07-05 16:10:58 -0300

    P^rimer commit desafeCentrexTester

---
+++ condiciones_safeCentrex.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##Fijarse de no imprimir nada por pantalla salvo el resultado ECHO
+
+check_AsteriskUp(){
+	pidof_asterisk=$(pidof asterisk)
+	if [[ -z pidof_asterisk ]]; then
+		##echo "No hay una proceso asterisk ejecutàndose"
+		echo 0
+	fi
+	echo 1
+}
+
+check_SafeCentrexUp(){
+	##ps -aux | grep 'safe_centrex' | grep 'root'
+	wc_ps_aux=$(ps -aux | grep 'safe_centrex' | wc -l)
+	if [[ $wc_ps_aux -ne 2 ]]; then
+		##echo "ERROR = Safe centrex no se encuentra corriendo"
+		echo 0
+	fi
+	echo 1
+}
+
+check_AsteriskAndSafeCentrexUp(){
+	result1=check_AsteriskUp
+	result2=check_SafeCentrexUp
+	echo $(($result1&&$result2))
+}
+
+
+check_AsteriskListeningOnPort5060Udp(){
+	##	Verifica si al menos hay uno
+	##	TODO = VER SI ES NECESARIO QUE COMPRUEBE QUE HAY SÓLO UN ASTERISK ESCUCHANDO EN ESE PUERTO
+
+	cantidad_ast_en_5060=$(netstat -putan | grep 'udp' | grep 'asterisk' | awk '{print $4}' | awk -F ":" '{print $2}' | grep '5060' | wc -l)
+	if [[ cantidad_ast_en_5060 ]]; then
+		echo 1
+	else
+		echo 0	
+	fi	
+}
---
+++ safeCentrex_Tester.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##	IMPORTANTE	CORRERLO COMO SUDO, SINO NO CORRE LOS KILLALL Y PS -PUTAN
+##	HACER UNA GUÍA DE LOS PARÁMETROS RECIBIDOS?????
+
+## TODO	=	Cambiar las variables de las funciones a local así no hay forma que se pisen el contenido
+
+## https://www.linuxjournal.com/content/return-values-bash-functions
+
+
+. /home/voipgroup/condiciones_safeCentrex.sh
+When_AsteriskCae5VecesMuySeguidas_Then_AsteriskSigueEstandoLevantando(){
+	##	Precondiciones antes de iniciar la prueba
+	asterisk_status=$(check_AsteriskUp)
+	safeCentrex_status=$(check_SafeCentrexUp)
+	echo "Estado del Asterisk		=" $asterisk_status
+	echo "Estado del Safe Centrex	=" $safeCentrex_status
+	initial_conditions=$(($asterisk_status&&$safeCentrex_status))
+	assertEquals "Asterisk o Safe Centrex no se encuentran corriendo"$initial_conditions 1
+
+	numero_backups_previos=$(ls -l /var/www/backups |  grep safe_centrex | wc -l)
+	
+	sudo date -s "10 minutes"
+	cantidad_caidas=5
+	for (( i = 0; i < cantidad_caidas; i++ )); do
+		killall -9 asterisk
+		sleep 20
+		asterisk_listening=$(check_AsteriskListeningOnPort5060Udp)
+		assertEquals "Asterisk no se encuentra escuchando en el puerto 5060 "asterisk_listening 1
+	done
+	##sudo date -s "10 minutes"
+	
+}
+
+
+
+When_Lararalara_Then_laralara(){
+	assertEquals 0 0
+}
+
+. /home/voipgroup/shunit2-2.1.7/shunit2
