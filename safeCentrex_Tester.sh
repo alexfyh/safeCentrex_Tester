5ece6d000981cb6b81b5702ef75a1c1beb0640fe
alexfyh <alexander.fyh@gmail.com>
2019-07-05 17:54:38 -0300

    En formato UNIX y comenzando los test con test_

--- safeCentrex_Tester.sh
+++ safeCentrex_Tester.sh
@@ -9,14 +9,14 @@
 
 
 . /home/voipgroup/git_safeCentrex/safeCentrex_Tester/condiciones_safeCentrex.sh
-When_AsteriskCae5VecesMuySeguidas_Then_AsteriskSigueEstandoLevantando(){
+test_When_AsteriskCae5VecesMuySeguidas_Then_AsteriskSigueEstandoLevantando(){
 	##	Precondiciones antes de iniciar la prueba
 	asterisk_status=$(check_AsteriskUp)
 	safeCentrex_status=$(check_SafeCentrexUp)
 	echo "Estado del Asterisk		=" $asterisk_status
 	echo "Estado del Safe Centrex	=" $safeCentrex_status
 	initial_conditions=$(($asterisk_status&&$safeCentrex_status))
-	assertEquals "Asterisk o Safe Centrex no se encuentran corriendo"$initial_conditions 1
+	assertEquals "Asterisk o Safe Centrex no se encuentran corriendo" $initial_conditions 1
 
 	numero_backups_previos=$(ls -l /var/www/backups |  grep safe_centrex | wc -l)
 	
@@ -26,16 +26,15 @@ When_AsteriskCae5VecesMuySeguidas_Then_AsteriskSigueEstandoLevantando(){
 		killall -9 asterisk
 		sleep 20
 		asterisk_listening=$(check_AsteriskListeningOnPort5060Udp)
-		assertEquals "Asterisk no se encuentra escuchando en el puerto 5060 "asterisk_listening 1
+		assertEquals "Asterisk no se encuentra escuchando en el puerto 5060 " $asterisk_listening 1
 	done
-	##sudo date -s "10 minutes"
-	
+	##sudo date -s "10 minutes"	
+}
+test1(){
+	echo "hola"
 }
 
-
-
-When_Lararalara_Then_laralara(){
-	assertEquals 0 0
+test2(){
+	echo "hola2"
 }
-
 . /home/voipgroup/shunit2-2.1.7/shunit2
